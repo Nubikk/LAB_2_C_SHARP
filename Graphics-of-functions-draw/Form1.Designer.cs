7e68495f9f5b9837103ca2a17bacf5f9862ebbd3
Nubikk <60070139+Nubikk@users.noreply.github.com>
2022-05-05 18:55:23 +0300

    afdfg
    asdg

---
+++ Graphics-of-functions-draw/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Graphics_of_functions_draw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y});
+            this.dataGridView1.Location = new System.Drawing.Point(112, 334);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(247, 149);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 305);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(457, 490);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Графики функций";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Graphics-of-functions-draw/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graphics_of_functions_draw
+{
+    
+    public partial class Form1 : Form
+    {
+        Chart myChart = new Chart();
+        Series mySeriesOfPoint = new Series("function");
+        public Form1()
+        {
+
+            InitializeComponent();
+           
+            //кладем его на форму и растягиваем на все окно.
+            myChart.Parent = this;
+            myChart.Dock = DockStyle.None;
+            myChart.Size = new Size (473,270);
+
+            //добавляем в Chart область для рисования графиков, их может быть
+            //много, поэтому даем ей имя.
+            myChart.ChartAreas.Add(new ChartArea("Math functions"));
+            //Создаем и настраиваем набор точек для рисования графика, в том
+            //не забыв указать имя области на которой хотим отобразить этот
+            //набор точек.
+         
+           
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+       
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mySeriesOfPoint.ChartType = SeriesChartType.FastLine;
+            mySeriesOfPoint.ChartArea = "Math functions";
+            mySeriesOfPoint.Points.Clear();
+            myChart.Series.Clear();
+            for (int i = 0 ; i < dataGridView1.RowCount - 1; i++)
+            {
+                double x = Convert.ToDouble(dataGridView1[0, i].Value);
+                double y = Convert.ToDouble(dataGridView1[1, i].Value);
+                mySeriesOfPoint.Points.AddXY(x, y);
+            }
+            //Добавляем созданный набор точек в Chart
+            myChart.Series.Add(mySeriesOfPoint);
+        }
+    }
+}
